5b480edbf334c14509a49b7d66ce46f0a27bc949
Bryan <bryan.duva@gmail.com>
2020-03-15 15:51:25 -0400

    update data store

--- src/data-store.ts
+++ src/data-store.ts
@@ -1,12 +1,57 @@
+import { Observable, isObservable } from 'rxjs';
 
-import { StateStore } from './state-store'
+import { StateStore, Modifier } from './state-store';
+import { map } from 'rxjs/operators';
+
 
 export interface KeyedData<T> {
     [key:string]: T
 }
 
+interface UpdatePayload<T> {
+    key: string;
+    data: T | Observable<T>;
+}
+
+
 export class DataStore<T> extends StateStore<KeyedData<T>> {
+
+    private keyedDataModifier<T>(state: KeyedData<T>, payload: UpdatePayload<T>) {
+        const mod = (v: T) => ({...state, ...{[payload.key]: v}});
+        return (isObservable(payload.data)) 
+            ? payload.data.pipe(map(v => mod(v))) 
+            : mod(payload.data);
+    }
+    
     constructor() {
-        super({})
+        super({});
+    }
+
+    get(key: string) {
+        return this.select(key);
+    }
+
+    set(key: string, data: T | Observable<T>) {
+        this.modify({key, data}, this.keyedDataModifier);
+    }
+
+    delete(key: string) {
+        this.set(key, undefined)
+    }
+
+    update<D>(key: string, modifier: Modifier<D, T>) {
+        const stateModifier = (state: KeyedData<T>, payload: Modifier<D,T>) => {
+            const item = state[key];
+            if (!item) {
+                throw new Error('cannot call update on non existent key');
+            }
+            const modItem = payload.modifier(item, payload.payload);
+            return this.keyedDataModifier(state, {key, data: modItem});
+        }
+        this.modify(modifier, stateModifier);
+    }
+
+    clear() {
+        this.next({});
     }
 }
