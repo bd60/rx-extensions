376c8d16e7302fc2feb7deeedfc3506c12ada0d0
Bryan <bryan.duva@gmail.com>
2020-03-20 19:46:27 -0400

    resource store

--- src/resource-store.ts
+++ src/resource-store.ts
@@ -11,9 +11,6 @@ export type KeyBuilder<D> = (arg: D) => string
 export interface ResourceStoreConfig<T, D> {
     initialState?: T;
     getResource: ResourceFunction<T, D>;
-    updateResource?: ResourceFunction<T, D>;
-    deleteResource?: ResourceFunction<T, D>;
-    createResource?: ResourceFunction<T, D>;
     keyBuilder?: KeyBuilder<D>;
 }
 
@@ -21,20 +18,10 @@ const defaultKeyBuilder = <D>(arg: D) => typeof arg === 'string' ? arg : JSON.st
 
 export abstract class AbstractResourceStore<T, D> extends DataStore<T> {
     getResource: ResourceFunction<T, D>
-    updateResource?: ResourceFunction<T, D>
-    deleteResource?: ResourceFunction<T, D>
-    createResource?: ResourceFunction<T, D>
     keyBuilder: KeyBuilder<D>;
 
     constructor(initialState: KeyedData<T> = {}) {
         super(initialState);
-    // constructor(config: ResourceStoreConfig<T,A>) {
-        // super(config.initialState || {});
-        // this.getResource = config.getResource;
-        // this.updateResource = config.updateResource;
-        // this.deleteResource = config.deleteResource;
-        // this.createResource = config.createResource;
-        // this.keyBuilder = config.keyBuilder || defaultKeyBuilder;
     }
 
     cacheGet(arg: D) {
@@ -50,31 +37,16 @@ export abstract class AbstractResourceStore<T, D> extends DataStore<T> {
         );
     }
 
-    cacheDelete(arg: D) {
-        if (!this.deleteResource) {
-            throw new Error('cacheDelete requires a delete resource function')
-        }
+    cacheSet(arg: D) {
         const key = this.keyBuilder(arg);
-        this.set(key, this.deleteResource(arg).pipe(mapTo(undefined)))
+        this.set(key, this.getResource(arg));
     }
+}
 
-    cacheUpdate(arg: D) {
-        if (!this.updateResource) {
-            throw new Error('cacheUpdate requires an update resource function')
-        }
-        const key = this.keyBuilder(arg);
-        const modifier: StateModifier<D, T> = (state: T, payload: D) => {
-            return this.updateResource(arg);
-        }
-        this.update(key, arg, modifier);
+export class ResourceStore<T, D> extends AbstractResourceStore<T, D> {
+    constructor(config: ResourceStoreConfig<T,D>) {
+        super(config.initialState || {});
+        this.getResource = config.getResource;
+        this.keyBuilder = config.keyBuilder || defaultKeyBuilder;
     }
-
-    cacheCreate(arg: D) {
-        if (!this.createResource) {
-            throw new Error('cacheCreate requires a create resource function')
-        }
-        const key = this.keyBuilder(arg);
-        this.set(key, this.createResource(arg))
-    }
-
 }
