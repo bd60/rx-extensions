7129153fcc9130a67b2b198ce9f6c56b2b874a59
Bryan <bryan.duva@gmail.com>
2020-03-26 00:48:13 -0400

    export the pull subject

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './state-store';
 export * from './data-store';
 export * from './resource-store';
 export * from './array-store';
-export * from './rx-utilities'
+export * from './pull-subject';
+export * from './rx-utilities';
